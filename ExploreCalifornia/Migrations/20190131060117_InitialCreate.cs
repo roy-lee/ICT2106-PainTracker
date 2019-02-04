b2c21ab7e8b92850275f9e29b55f75e91b47fd33
Roy Lee <1700500@sit.singaporetech.edu.sg>
2019-02-05 02:34:11 +0800

    initial commit - working CRUD with theme

---
+++ ExploreCalifornia/Migrations/20190131060117_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExploreCalifornia.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tour",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Length = table.Column<int>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    Rating = table.Column<string>(nullable: true),
+                    IncludesMeals = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tour", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tour");
+        }
+    }
+}
---
+++ ExploreCalifornia/Models/Tour.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExploreCalifornia.Models
+{
+    public class Tour
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Length { get; set; }
+        public decimal Price { get; set; }
+        public string Rating { get; set; }
+        public bool IncludesMeals { get; set; }
+    }
+}
