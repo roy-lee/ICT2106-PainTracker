da5c3ca9886379e8d37d5685fb6143b7a7a367c7
adventl <1701070@sit.singaporetech.edu.sg>
2019-02-09 17:49:50 +0800

    Baseline FollowUp Model

---
+++ PainTracker/Models/FollowUp/IFollowUpState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PainTracker.Models.FollowUp
+{
+    public interface IFollowUpState
+    {
+        void AskQuestion();
+        void AnswerQuestion();
+        void GenerateAdvice();
+        void CompleteFollowUp();
+    }
+}
---
+++ PainTracker/Models/FollowUp/PendingAdviceState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//TODO: Implement Methods
+namespace PainTracker.Models.FollowUp
+{
+    public class PendingAdviceState : IFollowUpState
+    {
+        public void AskQuestion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AnswerQuestion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GenerateAdvice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CompleteFollowUp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
